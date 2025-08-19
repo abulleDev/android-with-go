6cd01b32c690e0084647d4669be0a89866fd8486
abulle <olives2739@gmail.com>
2025-08-19 15:32:04 +0900

    feat: create example that uses Go module

--- app/src/main/java/com/example/androidwithgo/MainActivity.kt
+++ app/src/main/java/com/example/androidwithgo/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androidwithgo.ui.theme.AndroidWithGoTheme
+import gomodule.Gomodule
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidWithGoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Main(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +31,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Main(modifier: Modifier) {
+    var result by rememberSaveable { mutableStateOf("") }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidWithGoTheme {
-        Greeting("Android")
+    Column(modifier = modifier.padding(16.dp)) {
+        Text(
+            text = "Hello Go!",
+            style = MaterialTheme.typography.headlineLarge
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Text(text = result)
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Button(onClick = {
+            result = "IntVar = ${Gomodule.getIntVar()}"
+        }) { Text(text = "print(IntVar)") }
     }
 }
